76c7472c4e77e024e5ed0b69fd3ca5a7dde7eb98
Julian Fell <hi@jtfell.com>
2022-05-31 10:24:39 +1000

    add redirect

--- aunty.config.js
+++ aunty.config.js
@@ -40,5 +40,22 @@ module.exports = {
     };
 
     return config;
-  }
+  },
+  deploy: [
+    {
+      to: '/www/res/sites/news-projects/<name>/<id>'
+    },
+    config => {
+      fs.writeFileSync(
+        path.join(__dirname, 'redirect', 'index.js'),
+        `window.location = String(window.location).replace('/latest/', '/${config.id}/')`
+      );
+
+      return {
+        ...config,
+        from: 'redirect',
+        to: '/www/res/sites/news-projects/<name>/latest'
+      };
+    }
+  ]
 };
